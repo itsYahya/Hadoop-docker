3fc109f0bb920e30ea8d13a0593ae9d370cb9832
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-01-13 18:10:12 +0100

    fix: the package manager apt will be configured in the docker file instead of the script

--- hadoop/tools/script.sh
+++ hadoop/tools/script.sh
@@ -6,10 +6,4 @@ $HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
 
 hdfs --daemon start datanode
 
-echo -e "deb http://archive.debian.org/debian stretch main\ndeb http://archive.debian.org/debian-security stretch/updates main" > /etc/apt/sources.list
-
-apt update -y && apt upgrade -y
-
-apt install python -y
-
 tail -f
