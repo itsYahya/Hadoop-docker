d0c515d58ef69afe5f64954cb1c8676f91689997
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-01-13 17:54:08 +0100

    fix: starting the datanode in the start of the container

--- hadoop/tools/script.sh
+++ hadoop/tools/script.sh
@@ -4,8 +4,10 @@ yes | $HADOOP_HOME/bin/hdfs namenode -format
 
 $HADOOP_HOME/sbin/hadoop-daemon.sh start namenode
 
+hdfs --daemon start datanode
+
 echo -e "deb http://archive.debian.org/debian stretch main\ndeb http://archive.debian.org/debian-security stretch/updates main" > /etc/apt/sources.list
-   
+
 apt update -y && apt upgrade -y
 
 apt install python3 -y
